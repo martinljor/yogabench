35603a5fd70db9629c3b2eac737462252679821e
Martin Jorge <martin.jorge@veeam.com>
2026-08-06 16:51:41 -0300

    Version 0.3.0-alpha

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 )
 
 // version del binario (se muestra en el banner de arranque y se etiqueta en el release).
-const version = "0.2.9-alpha"
+const version = "0.3.0-alpha"
 
 func main() {
 	port := flag.String("port", "8000", "puerto HTTP")
